524ccd9b00085b9bc8986f5a599c7e8a6fbb3919
miguel <riverosmejiamiguel@gmail.com>
2025-03-28 23:10:37 -0500

    aaaa

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,22 @@
-import  Express, { Application }  from "express";
-import morgan  from "morgan";
-import cors  from "cors";
+import Express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 import UserRoutes from "./routes/user_routes";
-
 import AppointmentRoutes from "./routes/appointment_router";
 
-const server:Application = Express();
-
+const server: Application = Express();
 
 server.use(Express.json());
 server.use(morgan("dev"));
 server.use(cors());
 
-
 server.use(UserRoutes);
-
 server.use(AppointmentRoutes);
+
+// Ruta para la raíz, evita el error 404 en Render
+server.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
 
 export default server;
