411ebe1f82ff256ba261d1be78ad03a77322daa2
miguel <riverosmejiamiguel@gmail.com>
2025-03-28 23:57:11 -0500

    aaaa

--- src/services/Credential/validateCredentialS.ts
+++ src/services/Credential/validateCredentialS.ts
@@ -1,16 +1,19 @@
-import { Credential } from '../../entities/Credential';
-import credentialRepository from '../../repositories/CredentialRepository';
+import { Credential } from "../../entities/Credential";
+import credentialRepository from "../../repositories/CredentialRepository";
 
-export const validateCredentialS = async (username: string, password: string): Promise<Credential | null> => {
-    // Buscar la credencial en la base de datos
-    const credential = await credentialRepository.findOne({
-        where: { username }
-    });
+export const validateCredentialS = async (
+  username: string,
+  password: string
+): Promise<Credential | null> => {
+  // Buscar la credencial en la base de datos
+  const credential = await credentialRepository.findOne({
+    where: { username },
+  });
 
-    // Verificar si la credencial existe y si la contraseña coincide
-    if (credential && credential.password === password) {
-        return credential; // Retornar el username si las credenciales son válidas
-    }
-    
-    return null; // Retornar null si no son válidas
+  // Verificar si la credencial existe y si la contraseña coincide
+  if (credential && credential.password === password) {
+    return credential; // Retornar el username si las credenciales son válidas
+  }
+
+  return null; // Retornar null si no son válidas
 };
